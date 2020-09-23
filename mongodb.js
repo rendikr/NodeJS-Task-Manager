f46d425166f808e6980a00c42e76979d55bc4adc
Rendi K <rendi.krinaldi@gmail.com>
2020-09-24 00:05:09 +0700

    connecting & inserting documents

---
+++ mongodb.js
@@ -0,0 +1,18 @@
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
+
+const connectionURL = 'mongodb://127.0.0.1:27017'
+const databaseName = 'task-manager'
+
+MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+  if (error) {
+    return console.error('Unable to connect to MongoDB database');
+  }
+
+  const db = client.db(databaseName)
+
+  db.collection('users').insertOne({
+    name: 'Rendi K.',
+    age: 30
+  })
+})
