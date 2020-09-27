c74fa31984da481d671d563ff385c7334e08643a
Rendi K <rendi.krinaldi@gmail.com>
2020-09-27 22:31:38 +0700

    deleting documents

--- mongodb.js
+++ mongodb.js
@@ -10,34 +10,17 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: 
 
   const db = client.db(databaseName)
 
-  // find from 'users' collection filtered by the search parameter and update it
-  db.collection('users').updateOne({
-    _id: new ObjectID('5f6e172be43fb13424cd86c7')
-  }, {
-    $set: {
-      name: 'Rendi' // change the value
-    },
-    $inc: {
-      age: 1 // change the value by incrementing it's value
-    }
+  // find from 'users' collection filtered by the search parameter and delete it
+  db.collection('users').deleteMany({
+    age: 6
   }).then((result) => {
     console.log(result)
   }).catch((error) => {
     console.error(error)
   })
 
-  // updatePromise.then((result) => {
-  //   console.log(result)
-  // }).catch((error) =>{
-  //   console.error(error)
-  // })
-
-  db.collection('tasks').updateMany({
-    completed: false
-  }, {
-    $set: {
-      completed: true
-    }
+  db.collection('tasks').deleteOne({
+    description: 'Buy arm rest'
   }).then((result) => {
     console.log(result)
   }).catch((error) => {
