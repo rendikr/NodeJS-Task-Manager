9d0175bde112a2afa65634e6608b953fb08b9f15
Rendi K <rendi.krinaldi@gmail.com>
2020-09-30 22:06:26 +0700

    resource deleting endpoint

--- src/index.js
+++ src/index.js
@@ -85,6 +85,20 @@ app.patch('/users/:id', async (req, res) => {
   }
 })
 
+app.delete('/users/:id', async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id)
+
+    if (!user) {
+      return res.status(404).send({ error: 'user not found' })
+    }
+
+    res.status(200).send(user)
+  } catch (e) {
+    res.status(500).send(e)
+  }
+})
+
 app.post('/tasks', async (req, res) => {
   // const task = new Task(req.body)
 
@@ -161,6 +175,20 @@ app.patch('/tasks/:id', async (req, res) => {
     res.status(200).send(task)
   } catch (e) {
     res.status(400).send(e)
+  }
+})
+
+app.delete('/tasks/:id', async (req, res) => {
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id)
+
+    if (!task) {
+      return res.status(404).send({ error: 'task not found' })
+    }
+
+    res.status(200).send(task)
+  } catch (e) {
+    res.status(500).send(e)
   }
 })
 
