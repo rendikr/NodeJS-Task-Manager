59c3708878570e2a473cecbccd0552a0fc563d98
Rendi K <rendi.krinaldi@gmail.com>
2020-10-14 08:50:22 +0700

    auto crop & image formatting new npm package: sharp

--- src/index.js
+++ src/index.js
@@ -6,16 +6,6 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use((req, res, next) => {
-  console.log(req.method, req.path)
-
-  next()
-})
-
-app.use((req, res, next) => {
-  res.status(503).send('Site under maintenance, please try again later')
-})
-
 app.use(express.json()) // automatically parse any json
 app.use(userRouter)
 app.use(taskRouter)
