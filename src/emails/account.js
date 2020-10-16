6ef10a92014cdbd4a2c53108be10eb8a6413e341
Rendi K <rendi.krinaldi@gmail.com>
2020-10-16 09:40:04 +0700

    environment variables

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,13 +1,13 @@
 const sgMail = require('@sendgrid/mail')
 
-const sendgridAPIkey = 'x'
+const sendgridAPIkey = process.env.SENDGRID_API_KEY
 
 sgMail.setApiKey(sendgridAPIkey)
 
 const sendWelcomeEmail = (email, name) => {
   sgMail.send({
     to: email,
-    from: 'my@mail.io',
+    from: process.env.SENDGRID_MAIL_FROM,
     subject: 'Thanks for joining in!',
     text: `Welcome to the app, ${name}. Let me know how you get along with the app.`
   })
@@ -16,7 +16,7 @@ const sendWelcomeEmail = (email, name) => {
 const sendCancellationEmail = (email, name) => {
   sgMail.send({
     to: email,
-    from: 'my@mail.io',
+    from: process.env.SENDGRID_MAIL_FROM,
     subject: 'Are you really leaving?',
     text: `It's so sad to see you leave, ${name}. How can we improve things to keep you on the app?`
   })
--- src/models/user.js
+++ src/models/user.js
@@ -60,7 +60,7 @@ const userSchema = new mongoose.Schema({
 // <schema>.methods.<methodName>
 userSchema.methods.generateAuthToken = async function () {
   const user = this
-  const token = jwt.sign({ _id: user._id.toString() }, 'thisismynewcourse')
+  const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET)
 
   user.tokens = user.tokens.concat({ token })
   await user.save()
